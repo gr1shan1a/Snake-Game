f34167274db680092b693699e09c85fa0fe5298c
Куранов Григорий <115718938+gr1shan1a@users.noreply.github.com>
2023-09-20 17:22:22 +0300

    Add files via upload

---
+++ src/MainWindow.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+    public MainWindow() {
+        setTitle("Змейка");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(320, 345);
+        add(new GameField());
+        setLocation(400, 400);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        MainWindow mw = new MainWindow();
+    }
+}
